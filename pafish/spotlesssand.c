dec07d948d69c425f5ed8b9d2b79867c90c786ab
Pete Merges <pdmerges@gmail.com>
2022-04-19 14:23:25 -0400

    added PoC Registry Code for USB device metric

--- pafish/spotlesssand.c
+++ pafish/spotlesssand.c
@@ -4,6 +4,25 @@
 #include "spotlesssand.h"
 
 int spotlesssand_clscount(){
+	HKEY regkey;
+	LONG ret;
+	int count = 0;
+	if(pafish_iswow64()){
+		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM/CurrentControlSet/Enum/USB", 0, KEY_READ | KEY_WOW64_64KEY, &regkey);
+	} else {
+
+		ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM/CurrentControlSet/Enum/USB", 0, KEY_READ, &regkey);
+	}
+	if (ret == ERROR_SUCCESS){
+		for (int i = 0; i < 200; i++){
+			ret = RegEnumKeyExA(regkey, i, NULL, 0, NULL, NULL);
+			if (ret != ERROR_SUCCESS){
+				count = i;
+				break;
+			}
+		}
+	}
+	printf("%s", count)	
 	return 0;
 }
 
